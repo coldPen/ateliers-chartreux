a3171a87c5e71ec088ba47485c71f431194c351a
Lucas <lucas.leroux@protonmail.com>
2019-09-05 16:40:57 +0200

    configuration contentful faite

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,8 +10,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `qtp43omz40l5`,
-        accessToken: `uf4QNNsP6fi9ssMiHj5ZfYjIgLGbhVuvB55ACBwn29s`
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
       }
     }
   ]

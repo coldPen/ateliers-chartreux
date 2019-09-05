8ce507ec39fe93b7cfcb045c080896294541f457
Lucas <lucas.leroux@protonmail.com>
2019-09-05 18:11:59 +0200

    environment variables now loaded correctly

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,15 +4,21 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   /* Your site config here */
   plugins: [
     {
       resolve: `gatsby-source-contentful`,
       options: {
+        // spaceId: "pqtp43omz40l5",
+        // accessToken: "uf4QNNsP6fi9ssMiHj5ZfYjIgLGbhVuvB55ACBwn29s",
         spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
-      }
-    }
-  ]
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+  ],
 }

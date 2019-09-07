140b4b77eb004d1e44b1158f505c23593a6928f2
Lucas <lucas.leroux@protonmail.com>
2019-09-08 00:50:20 +0200

    conventions scss adoptées + intro

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,28 @@
 import React from "react"
 import { Link } from "gatsby"
-import HeaderStyles from "./Header.module.scss"
+import {
+  header,
+  header__menu,
+  header__link,
+  header__logo,
+} from "./Header.module.scss"
 
 const Header = () => (
-  <header className={HeaderStyles.header}>
-    <nav className={HeaderStyles.header__menu}>
-      <Link to="/impression/" className={HeaderStyles.header__link}>
+  <header className={header}>
+    <nav className={header__menu}>
+      <Link to="/impression/" className={header__link}>
         Impression 2D/3D
       </Link>
-      <Link to="/informatique/" className={HeaderStyles.header__link}>
+      <Link to="/informatique/" className={header__link}>
         Informatique
       </Link>
-      <Link to="/" className={HeaderStyles.header__logo}>
+      <Link to="/" className={header__logo}>
         Ateliers Chartreux
       </Link>
-      <Link to="/audiovisuel/" className={HeaderStyles.header__link}>
+      <Link to="/audiovisuel/" className={header__link}>
         Audiovisuel
       </Link>
-      <Link to="/reparation/" className={HeaderStyles.header__link}>
+      <Link to="/reparation/" className={header__link}>
         Réparation
       </Link>
     </nav>
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,9 +1,9 @@
 import React from "react"
 import Header from "../Header"
-import LayoutStyles from "./Layout.module.scss"
+import { container } from "./Layout.module.scss"
 
 const Layout = ({ children }) => (
-  <div className={LayoutStyles.container}>
+  <div className={container}>
     <Header />
     <main>{children}</main>
   </div>

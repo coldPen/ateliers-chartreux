16d45d7762ca6e82ee01dbf883f981e855b13938
Lucas <lucas.leroux@protonmail.com>
2019-10-10 15:52:56 +0200

    enhanced header + changed bloglist grid gap

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,11 @@ import { Link } from "gatsby"
 import {
   header,
   header__menu,
+  header__list,
+  header__element,
+  header__element_left,
+  header__element_right,
+  header__element_center,
   header__link,
   header__logo,
 } from "./Header.module.scss"
@@ -11,21 +16,36 @@ import {
 export default () => (
   <header className={header}>
     <nav className={header__menu}>
-      <Link to="/impression/" className={header__link}>
-        Impression 2D/3D
-      </Link>
-      <Link to="/informatique/" className={header__link}>
-        Informatique
-      </Link>
-      <Link to="/" className={header__logo}>
-        <img src="https://via.placeholder.com/200x60" alt="Ateliers Chartreux" />
-      </Link>
-      <Link to="/audiovisuel/" className={header__link}>
-        Audiovisuel
-      </Link>
-      <Link to="/reparation/" className={header__link}>
-        Réparation
-      </Link>
+      <ul className={header__list}>
+        <li className={`${header__element} ${header__element_center}`}>
+          <Link to="/" className={header__logo}>
+            <img
+              src="https://via.placeholder.com/200x60"
+              alt="Ateliers Chartreux"
+            />
+          </Link>
+        </li>
+        <li className={`${header__element} ${header__element_left}`}>
+          <Link to="/impression/" className={header__link}>
+            Impression 2D/3D
+          </Link>
+        </li>
+        <li className={`${header__element} ${header__element_left}`}>
+          <Link to="/informatique/" className={header__link}>
+            Informatique
+          </Link>
+        </li>
+        <li className={`${header__element} ${header__element_right}`}>
+          <Link to="/audiovisuel/" className={header__link}>
+            Audiovisuel
+          </Link>
+        </li>
+        <li className={`${header__element} ${header__element_right}`}>
+          <Link to="/reparation/" className={header__link}>
+            Réparation
+          </Link>
+        </li>
+      </ul>
     </nav>
   </header>
 )

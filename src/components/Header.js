c1d0136af9bed421eb15f6128b59192a6fe75920
Lucas <lucas.leroux@protonmail.com>
2019-10-25 16:37:09 +0200

    enhanced navigation (no more JS fuckery with width detection)

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,9 +10,10 @@ import {
   header__list,
   header__list_expanded,
   header__element,
-  header__element_left,
-  header__element_right,
-  header__element_center,
+  header__element_1,
+  header__element_2,
+  header__element_3,
+  header__element_4,
   header__link,
   header__logo,
 } from "./Header.module.scss"
@@ -22,24 +23,24 @@ export default () => {
   const [menuIsExpanded, setExpand] = useState(false)
 
   // Get current width and return a boolean
-  const getWidthBool = () => {
-    const currentWidth =
-      typeof window !== "undefined"
-        ? window.innerWidth
-        : typeof document !== "undefined"
-        ? document.documentElement.clientWidth ||
-          document.getElementsByTagName("body")[0].clientWidth
-        : 0
-    console.log(currentWidth > 1200)
-    return currentWidth > 1200
-  }
+  // const getWidthBool = () => {
+  //   const currentWidth =
+  //     typeof window !== "undefined"
+  //       ? window.innerWidth
+  //       : typeof document !== "undefined"
+  //       ? document.documentElement.clientWidth ||
+  //         document.getElementsByTagName("body")[0].clientWidth
+  //       : 0
+  //   console.log(currentWidth > 1200)
+  //   return currentWidth > 1200
+  // }
 
   // Updated width type state
-  const [isWidthNormal, setWidthBool] = useState(getWidthBool())
+  // const [isWidthNormal, setWidthBool] = useState(getWidthBool())
 
-  const updateWidthBool = () => {
-    setWidthBool(getWidthBool())
-  }
+  // const updateWidthBool = () => {
+  //   setWidthBool(getWidthBool())
+  // }
 
   // Wrapper that detects outside clicks (for the mobile menu)
   const buttonRef = useRef(null)
@@ -60,13 +61,13 @@ export default () => {
   useEffect(() => {
     /* When component will mount and did mount */
     // Listen to the resize event
-    window.addEventListener("resize", updateWidthBool)
+    // window.addEventListener("resize", updateWidthBool)
     // Listen to outside-of-mobile-menu clicks
     document.addEventListener("mousedown", handleClickOutside)
 
     /* When component will unmount */
     return () => {
-      window.removeEventListener("resize", updateWidthBool)
+      // window.removeEventListener("resize", updateWidthBool)
       document.removeEventListener("mousedown", handleClickOutside)
     }
   })
@@ -74,14 +75,14 @@ export default () => {
   return (
     <header className={header}>
       <nav className={header__nav}>
-        {isWidthNormal ? null : (
-          <Link to="/" className={header__logo}>
-            <img
-              src="https://via.placeholder.com/240x72"
-              alt="Ateliers Chartreux"
-            />
-          </Link>
-        )}
+        {/* {isWidthNormal ? null : ( */}
+        <Link to="/" className={header__logo}>
+          <img
+            src="https://via.placeholder.com/240x72"
+            alt="Ateliers Chartreux"
+          />
+        </Link>
+        {/* )} */}
         <button
           aria-expanded={menuIsExpanded}
           aria-controls="menu"
@@ -98,7 +99,7 @@ export default () => {
           id="menu"
           ref={menuRef}
         >
-          {isWidthNormal ? (
+          {/* {isWidthNormal ? (
             <li className={`${header__element} ${header__element_center}`}>
               <Link to="/" className={header__logo}>
                 <img
@@ -107,23 +108,23 @@ export default () => {
                 />
               </Link>
             </li>
-          ) : null}
-          <li className={`${header__element} ${header__element_left}`}>
+          ) : null} */}
+          <li className={`${header__element} ${header__element_1}`}>
             <Link to="/impression/" className={header__link}>
               Impression 2D/3D
             </Link>
           </li>
-          <li className={`${header__element} ${header__element_left}`}>
+          <li className={`${header__element} ${header__element_2}`}>
             <Link to="/informatique/" className={header__link}>
               Informatique
             </Link>
           </li>
-          <li className={`${header__element} ${header__element_right}`}>
+          <li className={`${header__element} ${header__element_3}`}>
             <Link to="/audiovisuel/" className={header__link}>
               Audiovisuel
             </Link>
           </li>
-          <li className={`${header__element} ${header__element_right}`}>
+          <li className={`${header__element} ${header__element_4}`}>
             <Link to="/reparation/" className={header__link}>
               Réparation
             </Link>

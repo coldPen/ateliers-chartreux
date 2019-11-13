24a3192b70ae5953ceaaae1d44c9593d0098296e
Lucas <lucas.leroux@protonmail.com>
2019-11-13 18:34:07 +0100

    working on impression.js

--- src/pages/impression.js
+++ src/pages/impression.js
@@ -6,20 +6,51 @@ import SEO from "../components/SEO"
 import NonStretchedImage from "../components/NonStretchedImage"
 
 import "../reset.scss"
-import { impression3d, impression2d } from "./impression.module.scss"
+import {
+  impression,
+  impression__title,
+  impression__3d,
+  impression__2d,
+  impression__text,
+  impression__image,
+} from "./impression.module.scss"
 
-export default () => (
+export default ({ data }) => (
   <Layout>
     <SEO
-      title="Impression 2D / 3D"
+      title={data.contentfulImpression.titre}
       description="Présentation du pôle Impression 2D / 3D des Ateliers Chartreux"
       pathname="/impression"
     />
-    <section className={impression3d}>
-      <h1></h1>
-      <p></p>
-    </section>
-    <section></section>
+    <div className={impression}>
+      <h1 className={impression__title}>{data.contentfulImpression.titre}</h1>
+      <section className={impression__3d}>
+        <div
+          className={impression__text}
+          dangerouslySetInnerHTML={{
+            __html:
+              data.contentfulImpression.impression3d.childMarkdownRemark.html,
+          }}
+        />
+        <NonStretchedImage
+          className={impression__image}
+          fluid={data.contentfulImpression.image3d.fluid}
+        />
+      </section>
+      <section className={impression__2d}>
+        <div
+          className={impression__text}
+          dangerouslySetInnerHTML={{
+            __html:
+              data.contentfulImpression.impression2d.childMarkdownRemark.html,
+          }}
+        />
+        <NonStretchedImage
+          className={impression__image}
+          fluid={data.contentfulImpression.image2d.fluid}
+        />
+      </section>
+    </div>
   </Layout>
 )
 
